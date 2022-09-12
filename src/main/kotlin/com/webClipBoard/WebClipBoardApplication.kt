a4d19c7dbdf781ae8d5eea7ec3bd56e2082502f7
KimJung <jung.kim@voithru.com>
2022-09-12 15:47:56 +0900

    init

---
+++ src/main/kotlin/com/webClipBoard/WebClipBoardApplication.kt
@@ -0,0 +1,11 @@
+package com.webClipBoard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebClipBoardApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebClipBoardApplication>(*args)
+}
