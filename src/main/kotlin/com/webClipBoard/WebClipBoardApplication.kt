2d6d79e6183f821a0235528c10f2e78b29a747c0
esteban0829 <esteban0829@naver.com>
2022-10-17 20:39:40 +0900

    feat: auto security configuration excluded temporarily

--- src/main/kotlin/com/webClipBoard/WebClipBoardApplication.kt
+++ src/main/kotlin/com/webClipBoard/WebClipBoardApplication.kt
@@ -1,9 +1,10 @@
 package com.webClipBoard
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
 class WebClipBoardApplication
 
 fun main(args: Array<String>) {
