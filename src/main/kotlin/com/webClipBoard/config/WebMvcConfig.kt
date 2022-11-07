433a8b380008bd9827e7b220ccff12b819d0b891
cola314 <ubj0314@naver.com>
2022-11-07 23:38:30 +0900

    feat: setup bootstarp and thymeleaf layout

--- src/main/kotlin/com/webClipBoard/config/WebMvcConfig.kt
+++ src/main/kotlin/com/webClipBoard/config/WebMvcConfig.kt
@@ -8,11 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class WebMvcConfig: WebMvcConfigurer {
     override fun addViewControllers(registry: ViewControllerRegistry) {
         super.addViewControllers(registry)
-        registry.addViewController("/login").setViewName("login")
-        registry.addViewController("/signup").setViewName("signup")
-        registry.addViewController("/admin").setViewName("admin")
-        registry.addViewController("/").setViewName("main")
-        registry.addViewController("/file-upload").setViewName("file-upload")
-        registry.addViewController("/hello").setViewName("hello")
+        registry.addViewController("/login").setViewName("pages/login")
+        registry.addViewController("/signup").setViewName("pages/signup")
+        registry.addViewController("/admin").setViewName("pages/admin")
+        registry.addViewController("/").setViewName("pages/main")
+        registry.addViewController("/file-upload").setViewName("pages/file-upload")
+        registry.addViewController("/hello").setViewName("pages/hello")
     }
 }
