44688db1590c4e82856f6764efd9b7ecab131cde
esteban0829 <esteban0829@naver.com>
2022-11-11 13:53:19 +0900

    refac: unused import

--- src/main/kotlin/com/webClipBoard/WebClipBoardApplication.kt
+++ src/main/kotlin/com/webClipBoard/WebClipBoardApplication.kt
@@ -2,10 +2,8 @@ package com.webClipBoard
 
 import io.awspring.cloud.autoconfigure.context.ContextResourceLoaderAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 
-//@SpringBootApplication(exclude = [SecurityAutoConfiguration::class, ContextResourceLoaderAutoConfiguration::class])
 @SpringBootApplication(exclude = [ContextResourceLoaderAutoConfiguration::class])
 class WebClipBoardApplication
 
